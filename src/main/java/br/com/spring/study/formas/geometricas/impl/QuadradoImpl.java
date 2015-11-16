ebd33262baedac6ea4ffb90b80f4072c05c1ec53
brunofprusch <brunoprusch@gmail.com>
2015-11-16 21:57:52 -0200

    Change xml to use component-scan.

--- src/main/java/br/com/spring/study/formas/geometricas/impl/QuadradoImpl.java
+++ src/main/java/br/com/spring/study/formas/geometricas/impl/QuadradoImpl.java
@@ -1,7 +1,10 @@
 package br.com.spring.study.formas.geometricas.impl;
 
+import org.springframework.stereotype.Component;
+
 import br.com.spring.study.formas.geometricas.interfaces.Quadrado;
 
+@Component
 public class QuadradoImpl implements Quadrado {
 
 	public Integer area(Integer lado) {
--- src/main/java/br/com/spring/study/formas/geometricas/service/CalculadorService.java
+++ src/main/java/br/com/spring/study/formas/geometricas/service/CalculadorService.java
@@ -1,15 +1,16 @@
 package br.com.spring.study.formas.geometricas.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import br.com.spring.study.formas.geometricas.interfaces.Quadrado;
 
+@Component
 public class CalculadorService {
 
 	@Autowired
 	public Quadrado quadrado;
 
-	
 	public Integer calcularAreaQuadrado(Integer lado){
 		return quadrado.area(lado);
 	}
